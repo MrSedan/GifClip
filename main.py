b15eae47bb1ce883b8d2067777eeff1ea934975f
MrSedan <elpashevs@bk.ru>
2023-02-16 21:38:58 +0300

    Catching clicking on gifs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import gi, requests, dotenv, os
+import gi, requests, dotenv, os, sys
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -11,7 +11,13 @@ dotenv.load_dotenv()
 class EntryWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="Entry Demo")
-        self.set_size_request(200, 50)
+        
+        #TODO: Do working ui from UI builder
+        builder = Gtk.Builder()
+        try:
+            builder.add_from_file('./design.ui')
+        except:
+            sys.exit(1)
 
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(vbox)
@@ -23,17 +29,45 @@ class EntryWindow(Gtk.Window):
         
         self.button = Gtk.Button(label="Click me")
         self.button.connect("clicked", self.on_button_clicked)
+        self.images = [Gtk.Image.new_from_file('image.gif') for i in range(10)]
         vbox.pack_start(self.button, True, True, 0)
+        # make a grid and add images to it
+        self.grid = Gtk.Grid()
+        self.grid.set_row_spacing(6)
+        self.grid.set_column_spacing(6)
+        self.grid.set_margin_top(6)
+        self.grid.set_margin_bottom(6)
+        self.grid.set_margin_start(6)
+        self.grid.set_margin_end(6)
+        self.grid.set_column_homogeneous(True)
+        self.grid.set_row_homogeneous(True)
+        vbox.pack_start(self.grid, True, True, 0)
+        self.grid.show()
         
     def on_key_press(self, widget, event):
         print(event.keyval)
     
     def on_button_clicked(self, widget):
         print('Clicked')
-        # do a GET request to the Tenor api (tenor.googleapis.com) with an api from the .env file
-        url = f"https://tenor.googleapis.com/v2/search?q={self.entry.get_text()}&key={os.getenv('TENOR_API_KEY')}&client_key=my_test_app&limit=8&media_filter=tinygif,gif"
+        url = f"https://tenor.googleapis.com/v2/search?q={self.entry.get_text()}&key={os.getenv('TENOR_API_KEY')}&client_key=my_test_app&limit=10&media_filter=tinygif,gif,tinygifpreview"
         response = requests.get(url).json()
-        print(response['results'][0]['media_formats']['gif']['url'])
+        for i, img in enumerate(response['results']):
+            gif_url = img['media_formats']['tinygif']['url']
+            response = requests.get(gif_url)
+            # # write the image to a file
+            with open(f'./gifs/image{i}.gif', 'wb') as f:
+                f.write(response.content)
+            image = Gtk.Image.new_from_file(f'./gifs/image{i}.gif')
+            ev_box = Gtk.EventBox()
+            ev_box.add(image)
+            ev_box.show()
+            ev_box.connect("button-press-event", self.on_image_clicked)
+            self.grid.attach(ev_box, 0, i, 1, 1)
+            image.show()
+    
+    def on_image_clicked(self, widget, event):
+        print('Clicked on image...')
+        
         
 
 win = EntryWindow()
