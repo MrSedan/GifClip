803ce0b99c837ab7e9bfbd5605ede197a6193b95
Sergey Elpashev <elpashevs@bk.ru>
2023-03-14 13:12:52 +0300

    Copying gif url by backend

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,13 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { ipcRenderer, contextBridge } from "electron";
+
+contextBridge.exposeInMainWorld("electron", {
+    notificationApi: {
+        sendToClipboard(url: string) {
+            ipcRenderer.send("clip-gif", url);
+        },
+    },
+    batteryApi: {},
+    fileApi: {},
+});
