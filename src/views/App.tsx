f7511014b2e5df2edc95c5f7b1fb7767a19b77bc
Sergey Elpashev <elpashevs@bk.ru>
2023-03-18 22:22:58 +0300

    Notification about copying

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,11 +1,16 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import "./styles/app.css";
 
 export default function App() {
     const [gifs, setGifs] = useState<string[]>([]);
+    const [activeNotification, setActiveNotification] = useState(false);
 
     async function copyImg(url: string) {
+        setActiveNotification(true);
+        setTimeout(() => {
+            setActiveNotification(false);
+        }, 3000);
         return electron.notificationApi.sendToClipboard(url);
     }
     return (
@@ -29,6 +34,12 @@ export default function App() {
                 {gifs.length == 2 && gifs[0] == "error" && (
                     <p>Error: {gifs[1]}!</p>
                 )}
+            </div>
+            <div
+                id="message-box"
+                className={activeNotification ? "active" : ""}
+            >
+                Successfully copied!
             </div>
         </div>
     );
